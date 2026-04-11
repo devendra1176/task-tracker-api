5dbf4a6680405e9438a4900971dd70035d48b9a1
devendra1176 <devendra1176sahu@gmail.com>
2026-04-11 13:51:30 +0530

    update SecurityConfig

--- src/main/java/com/project/tasktracker/config/SecurityConfig.java
+++ src/main/java/com/project/tasktracker/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.project.tasktracker.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/",
+                                "/error",
+                                "/auth/signup",
+                                "/auth/login",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
