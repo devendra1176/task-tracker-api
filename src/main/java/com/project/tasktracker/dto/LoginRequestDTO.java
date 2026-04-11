daa6d58dcb043e4ac76c76a53829444015fb319c
devendra1176 <devendra1176sahu@gmail.com>
2026-04-11 12:07:54 +0530

    Add LoginRequestDTO

---
+++ src/main/java/com/project/tasktracker/dto/LoginRequestDTO.java
@@ -0,0 +1,22 @@
+package com.project.tasktracker.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please enter a valid email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public LoginRequestDTO() {
+    }
+    
+}
