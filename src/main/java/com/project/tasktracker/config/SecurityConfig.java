1129ac4ce128a42b73d7febd031cf1bb21d4572c
devendra1176 <devendra1176sahu@gmail.com>
2026-05-19 16:08:56 +0530

    fix(security): update allowed origin for CORS configuration

--- src/main/java/com/project/tasktracker/config/SecurityConfig.java
+++ src/main/java/com/project/tasktracker/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://task-tracker-frontend-devendra1176s-projects.vercel.app"
+                "https://tasktracker.app.devendra.indevs.in"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
